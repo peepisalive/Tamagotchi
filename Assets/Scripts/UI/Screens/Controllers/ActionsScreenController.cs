65179c04b1db08ab507e6d606bd04e908eb8169d
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-12-24 01:07:35 +0700

    edit actions screen

--- Assets/Scripts/UI/Screens/Controllers/ActionsScreenController.cs
+++ Assets/Scripts/UI/Screens/Controllers/ActionsScreenController.cs
@@ -1,6 +1,7 @@
 using Application = Tamagotchi.Application;
 using Modules.Navigation;
 using UI.Screen.View;
+using UI.Controller;
 using UnityEngine;
 
 namespace UI.Screen.Controller
@@ -8,7 +9,9 @@ namespace UI.Screen.Controller
     [RequireComponent(typeof(ActionsScreenView))]
     public sealed class ActionsScreenController : ScreenController
     {
+        [Header("View")]
         [SerializeField] private ActionsScreenView _view;
+        [SerializeField] private NavigationButtonController _buttonPrefab;
         [SerializeField] private RectTransform _buttonsParent;
 
         private NavigationBlock _navigationBlock;
@@ -21,9 +24,17 @@ namespace UI.Screen.Controller
             if (_navigationBlock == null || _navigationPoint == null)
                 return;
 
-            foreach (var i in _navigationBlock.NavigationChain)
+            foreach (var navigationPoint in _navigationBlock.NavigationChain)
             {
+                if (navigationPoint.Type == _navigationPoint.Type)
+                    _navigationPoint = navigationPoint;
+            }
 
+            var points = Application.Model.GetChildPointsOfType(_navigationBlock.Type, _navigationPoint.Type);
+
+            foreach (var point in points)
+            {
+                Instantiate(_buttonPrefab, _buttonsParent);
             }
         }
 
