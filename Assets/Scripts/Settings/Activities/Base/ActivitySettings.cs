0a1397bbe7b90ba39da911be34fcea4931c8abc8
lsd7nk <87329383+lsd7nk@users.noreply.github.com>
2024-02-19 01:55:03 +0700

    activities localization

--- Assets/Scripts/Settings/Activities/Base/ActivitySettings.cs
+++ Assets/Scripts/Settings/Activities/Base/ActivitySettings.cs
@@ -1,10 +1,29 @@
+using Modules.Localization;
 using Modules.Navigation;
 using UnityEngine;
+using System;
 
 namespace Settings.Activity
 {
     public abstract class ActivitySettings : ScriptableObject
     {
+        [field: SerializeField] public ActivityLocalization Localization { get; private set; }
         public abstract NavigationElementType Type { get; }
+    }
+
+
+    [Serializable]
+    public sealed class ActivityLocalization
+    {
+        public string Title => LocalizationProvider.GetText("title");
+
+        public string MainContent => LocalizationProvider.GetText("main_popup/content");
+        public string ResultContent => LocalizationProvider.GetText("result_popup/content");
+
+        public string LeftButtonContent => LocalizationProvider.GetText("button_content/left");
+        public string RightButtonContent => LocalizationProvider.GetText("button_content/right");
+
+
+        [SerializeField] private LocalizedText _asset;
     }
 }
