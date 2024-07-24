4f56ec9b6d6d14878f97911a08f46929586c1294
lsd7nk <87329383+lsd7nk@users.noreply.github.com>
2024-07-25 01:39:22 +0700

    minor edits

--- Assets/Scripts/UI/Screens/Controllers/LeaderboardScreenController.cs
+++ Assets/Scripts/UI/Screens/Controllers/LeaderboardScreenController.cs
@@ -1,7 +1,12 @@
+using UnityEngine;
+
 namespace UI.Controller.Screen
 {
     public sealed class LeaderboardScreenController : ScreenController
     {
+        [Header("Controller")]
+        [SerializeField] private ImageButtonController _updateButton;
+
         public override void Setup()
         {
             base.Setup();
