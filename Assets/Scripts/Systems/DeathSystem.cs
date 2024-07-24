7d548da048d7f8d12069f9e674ae36112e690029
lsd7nk <87329383+lsd7nk@users.noreply.github.com>
2024-07-25 01:10:18 +0700

    edit death system

--- Assets/Scripts/Systems/DeathSystem.cs
+++ Assets/Scripts/Systems/DeathSystem.cs
@@ -46,6 +46,11 @@ namespace Systems
             foreach (var i in _petFilter)
             {
                 _petFilter.Get1(i).Pet.Parameters.Get(ParameterType.Health).Add(1f);
+                _petFilter.Get1(i).Pet.Parameters.Get(ParameterType.Satiety).Add(1f);
+                _petFilter.Get1(i).Pet.Parameters.Get(ParameterType.Hygiene).Add(1f);
+                _petFilter.Get1(i).Pet.Parameters.Get(ParameterType.Happiness).Add(1f);
+                _petFilter.Get1(i).Pet.Parameters.Get(ParameterType.Fatigue).Add(-1f);
+
                 _petFilter.GetEntity(i).Del<DeadComponent>();
             }
 
